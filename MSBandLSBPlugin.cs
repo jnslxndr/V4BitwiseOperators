74c8c2b116e556ee778f58393f78122792a4d60f
jens-a-e <jens@ififelse.net>
2011-10-28 23:08:05 +0200

    Adding more Bit Operations and FastBit Converter

--- MSBandLSBPlugin.cs
+++ MSBandLSBPlugin.cs
@@ -19,24 +19,25 @@ namespace VVVV.Nodes
 	{
 		#region fields & pins
 		[Input("Input", DefaultValue = 1.0)]
-		ISpread<int> FInput;
-
+		IDiffSpread<int> FInput;
+		
 		[Output("MSB")]
 		ISpread<int> FOutput;
 		#endregion fields & pins
-
+		
 		//called when data for any output pin is requested
 		public void Evaluate(int SpreadMax)
 		{
+			if(!FInput.IsChanged) return;
 			FOutput.SliceCount = SpreadMax;
-
+			
 			for (int i = 0; i < SpreadMax; i++)
-				FOutput[i] = (byte)((((int)FInput[i])>>7)&0xff);
-
+			FOutput[i] = (byte)((((int)FInput[i])>>7)&0xff);
+			
 		}
 	}
-
-
+	
+	
 	#region PluginInfo
 	[PluginInfo(Name = "LSB", Category = "Value", Version = "1", Help = "Basic template with one value in/out", Tags = "")]
 	#endregion PluginInfo
@@ -44,24 +45,25 @@ namespace VVVV.Nodes
 	{
 		#region fields & pins
 		[Input("Input", DefaultValue = 1.0)]
-		ISpread<int> FInput;
-
+		IDiffSpread<int> FInput;
+		
 		[Output("LSB")]
 		ISpread<int> FOutput;
 		#endregion fields & pins
-
+		
 		//called when data for any output pin is requested
 		public void Evaluate(int SpreadMax)
 		{
+			if(!FInput.IsChanged) return;
 			FOutput.SliceCount = SpreadMax;
-
+			
 			for (int i = 0; i < SpreadMax; i++)
-				FOutput[i] = FInput[i]&0xFF;
-
+			FOutput[i] = FInput[i]&0xFF;
+			
 		}
 	}
-
-
+	
+	
 	#region PluginInfo
 	[PluginInfo(Name = "Shiftleft", Category = "Value", Version = "1", Help = "Basic template with one value in/out", Tags = "")]
 	#endregion PluginInfo
@@ -70,25 +72,25 @@ namespace VVVV.Nodes
 		#region fields & pins
 		[Input("Input", DefaultValue = 1.0)]
 		ISpread<int> FInput;
-
+		
 		[Input("Steps", DefaultValue = 0, IsSingle=true)]
 		ISpread<int> StepInput;
-
+		
 		[Output("Shifted")]
 		ISpread<int> FOutput;
 		#endregion fields & pins
-
+		
 		//called when data for any output pin is requested
 		public void Evaluate(int SpreadMax)
 		{
 			FOutput.SliceCount = SpreadMax;
-
+			
 			for (int i = 0; i < SpreadMax; i++)
-				FOutput[i] = FInput[i]<<StepInput[0];
-
+			FOutput[i] = FInput[i]<<StepInput[0];
+			
 		}
 	}
-
+	
 	#region PluginInfo
 	[PluginInfo(Name = "Shiftright", Category = "Value", Version = "1", Help = "Basic template with one value in/out", Tags = "")]
 	#endregion PluginInfo
@@ -96,24 +98,167 @@ namespace VVVV.Nodes
 	{
 		#region fields & pins
 		[Input("Input", DefaultValue = 1.0)]
-		ISpread<int> FInput;
-
+		IDiffSpread<int> FInput;
+		
 		[Input("Steps", DefaultValue = 0, IsSingle=true)]
-		ISpread<int> StepInput;
-
+		IDiffSpread<int> StepInput;
+		
 		[Output("Shifted")]
 		ISpread<int> FOutput;
 		#endregion fields & pins
-
+		
 		//called when data for any output pin is requested
 		public void Evaluate(int SpreadMax)
 		{
+			if(!FInput.IsChanged && !StepInput.IsChanged) return;
 			FOutput.SliceCount = SpreadMax;
-
+			
 			for (int i = 0; i < SpreadMax; i++)
-				FOutput[i] = FInput[i]>>StepInput[0];
-
+			FOutput[i] = FInput[i]>>StepInput[0];
+			
 		}
 	}
 	
+	#region PluginInfo
+	[PluginInfo(Name = "BitAND", Category = "Value", Version = "1", Help = "Basic template with one value in/out", Tags = "")]
+	#endregion PluginInfo
+	public class BitAND : IPluginEvaluate
+	{
+		#region fields & pins
+		[Input("Input", DefaultValue = 0)]
+		IDiffSpread<int> FInput;
+		
+		[Input("Input2", DefaultValue = 0)]
+		IDiffSpread<int> FInput2;
+		
+		[Output("Result")]
+		ISpread<int> FOutput;
+		#endregion fields & pins
+		
+		//called when data for any output pin is requested
+		public void Evaluate(int SpreadMax)
+		{
+			if(!FInput.IsChanged && !FInput2.IsChanged) return;
+			FOutput.SliceCount = SpreadMax;
+			
+			for (int i = 0; i < SpreadMax; i++)
+			FOutput[i] = FInput[i]&FInput2[0];
+			
+		}
+	}
+	
+	#region PluginInfo
+	[PluginInfo(Name = "BitOR", Category = "Value", Version = "1", Help = "Basic template with one value in/out", Tags = "")]
+	#endregion PluginInfo
+	public class BitOR : IPluginEvaluate
+	{
+		#region fields & pins
+		[Input("Input", DefaultValue = 0)]
+		IDiffSpread<int> FInput;
+		
+		[Input("Input2", DefaultValue = 0)]
+		IDiffSpread<int> FInput2;
+		
+		[Output("Result")]
+		ISpread<int> FOutput;
+		#endregion fields & pins
+		
+		//called when data for any output pin is requested
+		public void Evaluate(int SpreadMax)
+		{
+			if(!FInput.IsChanged && !FInput2.IsChanged) return;
+			FOutput.SliceCount = SpreadMax;
+			
+			for (int i = 0; i < SpreadMax; i++)
+			FOutput[i] = FInput[i]|FInput2[0];
+			
+		}
+	}
+
+	
+	#region PluginInfo
+	[PluginInfo(Name = "BitNOT", Category = "Value", Version = "1", Help = "Basic template with one value in/out", Tags = "")]
+	#endregion PluginInfo
+	public class BitNOT : IPluginEvaluate
+	{
+		#region fields & pins
+		[Input("Input", DefaultValue = 0)]
+		IDiffSpread<int> FInput;
+		
+		[Output("Result")]
+		ISpread<int> FOutput;
+		#endregion fields & pins
+		
+		//called when data for any output pin is requested
+		public void Evaluate(int SpreadMax)
+		{
+			if(!FInput.IsChanged) return;
+			FOutput.SliceCount = SpreadMax;
+			
+			for (int i = 0; i < SpreadMax; i++)
+			FOutput[i] = ~FInput[i];
+			
+		}
+	}
+
+	#region PluginInfo
+	[PluginInfo(Name = "BitXOR", Category = "Value", Version = "1", Help = "Basic template with one value in/out", Tags = "")]
+	#endregion PluginInfo
+	public class BitXOR : IPluginEvaluate
+	{
+		#region fields & pins
+		[Input("Input", DefaultValue = 0)]
+		IDiffSpread<int> FInput;
+		
+		[Input("Input2", DefaultValue = 0)]
+		IDiffSpread<int> FInput2;
+		
+		[Output("Result")]
+		ISpread<int> FOutput;
+		#endregion fields & pins
+		
+		//called when data for any output pin is requested
+		public void Evaluate(int SpreadMax)
+		{
+			if(!FInput.IsChanged && !FInput2.IsChanged) return;
+			FOutput.SliceCount = SpreadMax;
+			for (int i = 0; i < SpreadMax; i++)
+				FOutput[i] = FInput[i] ^ FInput2[i];
+			
+		}
+	}
+
+		#region PluginInfo
+	[PluginInfo(Name = "FastBit", Category = "Value", Version = "1", Help = "Basic template with one value in/out", Tags = "")]
+	#endregion PluginInfo
+	public class FastBit : IPluginEvaluate
+	{
+		#region fields & pins
+		[Input("Input", DefaultValue = 0)]
+		IDiffSpread<int> FInput;
+		
+		[Input("BitSize", DefaultValue = 8,IsSingle = true)]
+		ISpread<int> BitSize;
+		
+		[Output("Result")]
+		ISpread<int> FOutput;
+		#endregion fields & pins
+		
+		//called when data for any output pin is requested
+		public void Evaluate(int SpreadMax)
+		{
+			if(!FInput.IsChanged) return;
+			FOutput.SliceCount = SpreadMax * BitSize[0];
+			for (int i = 0; i < FOutput.SliceCount; i++)
+			{
+				int bi=0;
+				for (; bi<BitSize[0]; bi++)
+					FOutput[bi+(i*BitSize[0])] = (FInput[i]>>bi)&0x01;
+			}
+			
+			
+		}
+	}
+
+
 }
