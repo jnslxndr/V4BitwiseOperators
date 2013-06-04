2fba073f417c7bfdd3091b62e4c1374dc1d655f4
jens-a-e <jens@lea.io>
2013-06-04 14:26:22 +0200

    Fixed some spread issues and used correct naming for operands like &, |,^, etc

--- BitOperators.cs
+++ BitOperators.cs
@@ -37,7 +37,7 @@ namespace VVVV.Nodes
 			shift = (9-NumBits[0])-1;
 			mask  = 0xff>>shift;
 			for (int i = 0; i < SpreadMax; i++)
-			FOutput[i] = (FInput[i]>>NumBits[0])& mask;
+			FOutput[i] = (FInput[i]>>NumBits[i])& mask;
 			
 		}
 	}
@@ -103,7 +103,7 @@ namespace VVVV.Nodes
 			for (int i = 0; i < FOutput.SliceCount; i++)
 			{
 				int temp = MSB[i] & mask;
-				temp = temp << NumBits[0];
+				temp = temp << NumBits[i];
 				temp = temp | (LSB[i]&mask);
 				FOutput[i] = temp;
 			}
@@ -112,15 +112,15 @@ namespace VVVV.Nodes
 	}
 	
 	#region PluginInfo
-	[PluginInfo(Name = "Shiftleft", Category = "Value", Version = "1", Help = "Basic template with one value in/out", Tags = "")]
+	[PluginInfo(Name = "<<", Category = "Value", Version = "1", Help = "Basic template with one value in/out", Tags = "")]
 	#endregion PluginInfo
 	public class Shiftleft : IPluginEvaluate
 	{
 		#region fields & pins
-		[Input("Input", DefaultValue = 1.0)]
+		[Input("Input", DefaultValue = 0)]
 		ISpread<int> FInput;
 		
-		[Input("Steps", DefaultValue = 0, IsSingle=true)]
+		[Input("Steps", DefaultValue = 0)]
 		ISpread<int> StepInput;
 		
 		[Output("Shifted")]
@@ -133,21 +133,21 @@ namespace VVVV.Nodes
 			FOutput.SliceCount = SpreadMax;
 			
 			for (int i = 0; i < SpreadMax; i++)
-			FOutput[i] = FInput[i]<<StepInput[0];
+				FOutput[i] = FInput[i]<<StepInput[i];
 			
 		}
 	}
 	
 	#region PluginInfo
-	[PluginInfo(Name = "Shiftright", Category = "Value", Version = "1", Help = "Basic template with one value in/out", Tags = "")]
+	[PluginInfo(Name = ">>", Category = "Value", Version = "1", Help = "Basic template with one value in/out", Tags = "")]
 	#endregion PluginInfo
 	public class Shiftright : IPluginEvaluate
 	{
 		#region fields & pins
-		[Input("Input", DefaultValue = 1.0)]
+		[Input("Input", DefaultValue = 0)]
 		IDiffSpread<int> FInput;
 		
-		[Input("Steps", DefaultValue = 0, IsSingle=true)]
+		[Input("Steps", DefaultValue = 0)]
 		IDiffSpread<int> StepInput;
 		
 		[Output("Shifted")]
@@ -161,13 +161,13 @@ namespace VVVV.Nodes
 			FOutput.SliceCount = SpreadMax;
 			
 			for (int i = 0; i < SpreadMax; i++)
-			FOutput[i] = FInput[i]>>StepInput[0];
+				FOutput[i] = FInput[i]>>StepInput[i];
 			
 		}
 	}
 	
 	#region PluginInfo
-	[PluginInfo(Name = "BitAND", Category = "Value", Version = "1", Help = "Basic template with one value in/out", Tags = "")]
+	[PluginInfo(Name = "&", Category = "Value", Version = "1", Help = "Basic template with one value in/out", Tags = "")]
 	#endregion PluginInfo
 	public class BitAND : IPluginEvaluate
 	{
@@ -189,13 +189,38 @@ namespace VVVV.Nodes
 			FOutput.SliceCount = SpreadMax;
 			
 			for (int i = 0; i < SpreadMax; i++)
-			FOutput[i] = FInput[i]&FInput2[0];
+			FOutput[i] = FInput[i]&FInput2[i];
+			
+		}
+	}
+	#region PluginInfo
+	[PluginInfo(Name = "&", Category = "Value", Version = "Spectral", Help = "Basic template with one value in/out", Tags = "")]
+	#endregion PluginInfo
+	public class BitANDSpectral : IPluginEvaluate
+	{
+		#region fields & pins
+		[Input("Input", DefaultValue = 0)]
+		IDiffSpread<int> FInput;
+		
+		[Output("Result")]
+		ISpread<int> FOutput;
+		#endregion fields & pins
+		
+		//called when data for any output pin is requested
+		public void Evaluate(int SpreadMax)
+		{
+			if(!FInput.IsChanged) return;
+			FOutput.SliceCount = 1;
+			int result = 0;
+			for (int i = 0; i < SpreadMax; i++)
+				result &= FInput[i];
+			FOutput[0] = result;
 			
 		}
 	}
 	
 	#region PluginInfo
-	[PluginInfo(Name = "BitOR", Category = "Value", Version = "1", Help = "Basic template with one value in/out", Tags = "")]
+	[PluginInfo(Name = "|", Category = "Value", Version = "1", Help = "Basic template with one value in/out", Tags = "")]
 	#endregion PluginInfo
 	public class BitOR : IPluginEvaluate
 	{
@@ -217,14 +242,40 @@ namespace VVVV.Nodes
 			FOutput.SliceCount = SpreadMax;
 			
 			for (int i = 0; i < SpreadMax; i++)
-			FOutput[i] = FInput[i]|FInput2[0];
+			FOutput[i] = FInput[i]|FInput2[i];
 			
 		}
 	}
-	
+
+		#region PluginInfo
+	[PluginInfo(Name = "|", Category = "Value", Version = "Spectral", Help = "Basic template with one value in/out", Tags = "")]
+	#endregion PluginInfo
+	public class BitOrSpectral : IPluginEvaluate
+	{
+		#region fields & pins
+		[Input("Input", DefaultValue = 0)]
+		IDiffSpread<int> FInput;
+		
+		[Output("Result")]
+		ISpread<int> FOutput;
+		#endregion fields & pins
+		
+		//called when data for any output pin is requested
+		public void Evaluate(int SpreadMax)
+		{
+			if(!FInput.IsChanged) return;
+			FOutput.SliceCount = 1;
+			int result = 0;
+			for (int i = 0; i < SpreadMax; i++)
+				result |= FInput[i];
+			FOutput[0] = result;
+			
+		}
+	}
+
 	
 	#region PluginInfo
-	[PluginInfo(Name = "BitNOT", Category = "Value", Version = "1", Help = "Basic template with one value in/out", Tags = "")]
+	[PluginInfo(Name = "~", Category = "Value", Version = "1", Help = "Basic template with one value in/out", Tags = "")]
 	#endregion PluginInfo
 	public class BitNOT : IPluginEvaluate
 	{
@@ -249,7 +300,7 @@ namespace VVVV.Nodes
 	}
 	
 	#region PluginInfo
-	[PluginInfo(Name = "BitXOR", Category = "Value", Version = "1", Help = "Basic template with one value in/out", Tags = "")]
+	[PluginInfo(Name = "^", Category = "Value", Version = "1", Help = "Basic template with one value in/out", Tags = "")]
 	#endregion PluginInfo
 	public class BitXOR : IPluginEvaluate
 	{
@@ -299,8 +350,8 @@ namespace VVVV.Nodes
 			for (int i = 0; i < FOutput.SliceCount; i++)
 			{
 				int bi=0;
-				for (; bi<BitSize[0]; bi++)
-				FOutput[bi+(i*BitSize[0])] = (FInput[i]>>bi)&0x01;
+				for (; bi<BitSize[i]; bi++)
+				FOutput[bi+(i*BitSize[i])] = (FInput[i]>>bi)&0x01;
 			}
 			
 			
